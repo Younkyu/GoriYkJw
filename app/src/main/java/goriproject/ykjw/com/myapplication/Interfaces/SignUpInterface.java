2cba04f5472d39f50672606980863179dace34ff
DESKTOP-1R8JQHP\Younkyu <gtv1523411>
2017-04-10 12:37:24 +0900

    04010_list 레이아웃 파일수정      -> 까만바탕색, 흰글씨로 수정, 레이팅별도 빨간색으로 바꾸려고 생각중      검색 필터링 알고리즘 처음부터 다시 짜는 중      -> 기존 : 버튼을 누르면 그 버튼 조건에 맞는 강의 모두 검색      바꾸려는 방식 : 버튼을누르면 필터링이 되는 방식으로 변경      또한 카테고리/지역별 검색이 모두 같은 함수로 작동하도록 변경

---
+++ app/src/main/java/goriproject/ykjw/com/myapplication/Interfaces/SignUpInterface.java
@@ -0,0 +1,20 @@
+package goriproject.ykjw.com.myapplication.Interfaces;
+
+import org.json.JSONObject;
+
+import retrofit2.Callback;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+/**
+ * Created by Younkyu on 2017-04-10.
+ */
+
+public interface SignUpInterface {
+
+    @FormUrlEncoded
+    @POST("member/signup")
+    public void signup(@Field("username") String username, @Field("password1") String password1, @Field("password2") String password2,
+                       @Field("name") String name, Callback<JSONObject> callback);
+}
